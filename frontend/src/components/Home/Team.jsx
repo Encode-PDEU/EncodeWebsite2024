d45d70aee49f1c4fddd4d27f2f07ff6fe57c14da
Aryan Randeriya <aryanranderiya1478@gmail.com>
2024-08-19 20:04:30 +0530

    update 3 files and delete 1 file

--- frontend/src/components/Home/Team.jsx
+++ frontend/src/components/Home/Team.jsx
@@ -3,10 +3,10 @@ import { Button } from "@nextui-org/button"
 
 export function TeamCard({ imgsrc = "https://github.com/aryanranderiya.png", name, position }) {
     return <div className="flex flex-col p-[2em] items-center gap-5 bg-[#00ff7b] bg-opacity-15 rounded-none">
-        <img src={imgsrc} className="w-[200px] h-[200px]" />
+        <img src={imgsrc} className="w-[200px] h-[200px] rounded-full" />
         <div className="flex flex-col items-center">
-            <span className="text-2xl">{name}</span>
-            <span className="text-lg text-foreground-400">{position}</span>
+            <span className="text-2xl helvetica">{name}</span>
+            <span className="text-lg text-foreground-400 minecraft">{position}</span>
         </div>
     </div>
 }
@@ -20,12 +20,11 @@ export default function Team() {
         </div>
 
         <div className="flex flex-wrap gap-3 md:max-w-[70vw] justify-center mb-4">
-            <TeamCard name="Anokhi Shah" position="President" />
-            <TeamCard name="Saumya Talwani" position="Vice President" />
-            <TeamCard name="Diya Mirani" position="Treasurer" />
-            <TeamCard name="Dhyan Shah" position="Technical Head" />
+            <TeamCard name="Anokhi Shah" position="President" imgsrc="https://i.imgur.com/2SOGOOG.png" />
+            <TeamCard name="Saumya Talwani" position="Vice President" imgsrc="https://i.imgur.com/yXbXGMx.jpeg" />
+            <TeamCard name="Diya Mirani" position="Treasurer" imgsrc="https://i.imgur.com/LYXsFsQ.jpeg" />
+            <TeamCard name="Dhyan Shah" position="Technical Head" imgsrc="https://i.imgur.com/PTQeHnF.png" />
         </div>
-
         <Button color="success" radius="none" size="lg" className="font-semibold text-lg">View All</Button>
 
     </section>
--- frontend/src/components/Home/UpcomingEvents.jsx
+++ frontend/src/components/Home/UpcomingEvents.jsx
@@ -1,25 +1,48 @@
 import { Button } from "@nextui-org/button"
 import { Chip } from "@nextui-org/chip"
 
-function EventCard() {
+function formatDate(isoDate) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(isoDate).toLocaleDateString(undefined, options);
+}
+
+function EventCard({ title, date, type = "Workshop",
+    description = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente neque ipsum repudiandae quod quibusdam dolorum aut`
+}) {
     return (
         <div className="flex flex-col bg-[#00ff7b] bg-opacity-10 p-5 gap-2 hover:translate-y-[-5px] transition-all outline-1 hover:outline
          outline-[#00ff7b]">
             <div className="flex flex-col pb-2">
-                <Chip color="success" className="mb-2">10/10/24</Chip>
-                <span className="text-2xl">Event Title</span>
-                <span className="text-foreground-500 max-w-[19vw]">
-                    Event Description
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente neque ipsum repudiandae quod quibusdam dolorum aut
-                </span>
+                <div className="flex justify-between pb-3 gap-11">
+                    <Chip color="success" className="mb-2" variant="dot" >{formatDate(date)}</Chip>
+                    <Chip color="success" className="mb-2" variant="flat"  >{type}</Chip>
+                </div>
+                <span className="text-2xl">{title}</span>
+                <span className="text-foreground-500 max-w-[19vw]">{description}</span>
             </div>
-
             <Button color="success" variant="flat" radius="none">Register</Button>
         </div>
     )
 }
 
 export default function UpcomingEvents() {
+
+    const events = [
+        {
+            title: "Web Security Basics",
+            date: "2025-01-24T00:00:00Z",
+            description: "",
+            type: "Workshop"
+        },
+        {
+            title: "MERN Stack Bootcamp",
+            date: "2025-02-08T00:00:00Z",
+            description: "",
+            type: "Workshop"
+        },
+    ];
+
+
     return (
         <section className="min-h-screen h-fit justify-center w-full flex flex-col items-center gap-6 ">
             <div className="flex flex-col items-center gap-2">
@@ -28,14 +51,9 @@ export default function UpcomingEvents() {
             </div>
 
             <div className="flex flex-wrap gap-4 justify-center">
-                <EventCard />
-                <EventCard />
-                <EventCard />
-                <EventCard />
-                <EventCard />
-                <EventCard />
-                <EventCard />
-                <EventCard />
+                {events.map((event, index) => (
+                    <EventCard key={index} title={event.title} date={event.date} description={event.description} type={event.type} />
+                ))}
             </div>
 
             <Button color="success" radius="none" size="lg" className="mt-2 font-semibold text-lg">View All Events</Button>
